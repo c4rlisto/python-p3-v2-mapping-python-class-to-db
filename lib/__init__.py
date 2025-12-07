068fcaa17ac346fab931280d6c16a064e4d83fcf
Carl Kiprono <carlkiprono00@gmail.com>
2025-12-07 22:49:26 +0300

    initial commit

--- lib/__init__.py
+++ lib/__init__.py
@@ -1,5 +1,5 @@
-# lib/config.py
 import sqlite3
 
+# Connection to the database
 CONN = sqlite3.connect('company.db')
 CURSOR = CONN.cursor()
--- lib/department.py
+++ lib/department.py
@@ -1,6 +1,5 @@
 from __init__ import CURSOR, CONN
 
-
 class Department:
 
     def __init__(self, name, location, id=None):
@@ -10,3 +9,56 @@ class Department:
 
     def __repr__(self):
         return f"<Department {self.id}: {self.name}, {self.location}>"
+
+    @classmethod
+    def create_table(cls):
+        """Create a new table to persist Department instances"""
+        sql = """
+            CREATE TABLE IF NOT EXISTS departments (
+                id INTEGER PRIMARY KEY,
+                name TEXT,
+                location TEXT
+            )
+        """
+        CURSOR.execute(sql)
+        CONN.commit()
+
+    @classmethod
+    def drop_table(cls):
+        """Drop the table that persists Department instances"""
+        sql = "DROP TABLE IF EXISTS departments;"
+        CURSOR.execute(sql)
+        CONN.commit()
+
+    def save(self):
+        """Insert a new row for this Department instance and update its id"""
+        sql = """
+            INSERT INTO departments (name, location)
+            VALUES (?, ?)
+        """
+        CURSOR.execute(sql, (self.name, self.location))
+        CONN.commit()
+        self.id = CURSOR.lastrowid
+
+    @classmethod
+    def create(cls, name, location):
+        """Create and save a new Department instance"""
+        department = cls(name, location)
+        department.save()
+        return department
+
+    def update(self):
+        """Update the table row corresponding to this Department instance"""
+        sql = """
+            UPDATE departments
+            SET name = ?, location = ?
+            WHERE id = ?
+        """
+        CURSOR.execute(sql, (self.name, self.location, self.id))
+        CONN.commit()
+
+    def delete(self):
+        """Delete the table row corresponding to this Department instance"""
+        sql = "DELETE FROM departments WHERE id = ?"
+        CURSOR.execute(sql, (self.id,))
+        CONN.commit()
